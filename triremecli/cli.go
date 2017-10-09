c652db890cf2deef99292844aa5d922f4dd46ca2
Dimitri Stiliadis <dstiliadis@users.noreply.github.com>
2017-10-09 09:18:46 -0700

    Follow Trireme host updates. (#25)

--- triremecli/cli.go
+++ triremecli/cli.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
-	"strings"
 
 	"go.uber.org/zap"
 
@@ -47,49 +46,7 @@ func ProcessArgs(arguments map[string]interface{}, processor enforcer.PacketProc
 }
 
 func processCmdArgs(arguments map[string]interface{}) error {
-
-	if arguments["run"].(bool) {
-
-		var command string
-		if value, ok := arguments["<command>"]; ok && value != nil {
-			command = value.(string)
-		}
-
-		var labels []string
-		if value, ok := arguments["--label"]; ok && value != nil {
-			labels = value.([]string)
-		}
-
-		var serviceName string
-		if value, ok := arguments["--service-name"]; ok && value != nil {
-			serviceName = value.(string)
-		}
-
-		var ports string
-		if value, ok := arguments["--ports"]; ok && value != nil {
-			ports = value.(string)
-		}
-
-		portsSlice := []string{"0"}
-		if len(ports) > 0 {
-			portsSlice = strings.Split(ports, ",")
-		}
-
-		var params []string
-		if value, ok := arguments["<params>"]; ok && value != nil {
-			params = append(params, value.([]string)...)
-		}
-
-		return systemdutil.ExecuteCommandWithParameters(command, params, "", serviceName, portsSlice, labels)
-	}
-
-	var cgroup string
-	if value, ok := arguments["<cgroup>"]; ok && value != nil {
-		cgroup = value.(string)
-	}
-
-	return systemdutil.ExecuteCommandWithParameters("", nil, cgroup, "", nil, nil)
-
+	return systemdutil.ExecuteCommandFromArguments(arguments)
 }
 
 // processDaemonArgs is responsible for creating a trireme daemon

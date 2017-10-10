0ff8651f0bbc8a528ee28f76e0314fdc2c19c465
Amit Limaye <amit@aporeto.com>
2017-10-09 19:01:25 -0700

    changed func signature

--- policyexample/policy.go
+++ policyexample/policy.go
@@ -109,7 +109,7 @@ func (p *CustomPolicyResolver) ResolvePolicy(context string, runtimeInfo policy.
 
 	excluded := []string{}
 
-	containerPolicyInfo := policy.NewPUPolicy(context, policy.Police, ingress, egress, nil, tagSelectors, identity, annotations, ipl, p.triremeNets, excluded, []string{})
+	containerPolicyInfo := policy.NewPUPolicy(context, policy.Police, ingress, egress, nil, tagSelectors, identity, annotations, ipl, p.triremeNets, excluded, []string{}, []string{})
 
 	return containerPolicyInfo, nil
 }

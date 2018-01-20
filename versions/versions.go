97312d3f313bf4b5cff8a1f2c682b74b0158a973
Marcus Heese <marcus.heese@gmail.com>
2018-01-19 16:18:21 -0800

    reverting versions.go file

--- versions/versions.go
+++ versions/versions.go
@@ -1,7 +1,7 @@
 package versions
 
 // VERSION is the version of Trireme-Example
-const VERSION = "0.11"
+const VERSION = "unset"
 
 // REVISION is the revision of Trireme-Example
-const REVISION = "154f98fb1d6683ac70e80d30d757d37269c065d4"
+const REVISION = "unset"
